1a8e2f24b58dd8f1c72de912b68f8fb92f791b62
Touko Haapanen <toukoh@Touko-MacBook-Pro.local>
2023-12-16 14:10:40 +0200

    initialised nn header and ran test

---
+++ include/NeuralNetwork.hpp
@@ -0,0 +1,15 @@
+#ifndef NEURALNETWORK_HPP
+#define NEURALNETWORK_HPP
+
+#include <iostream>
+#include <eigen/Eigen/Dense>
+#include "Layer.hpp"
+
+class NeuralNetwork {
+public:
+	NeuralNetwork(int depht); //maybe include optimizer here?
+
+	
+}
+
+#endif
